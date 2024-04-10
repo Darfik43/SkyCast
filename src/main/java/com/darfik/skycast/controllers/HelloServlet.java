c729764d08f7bc8a546e382ede1cec9bc4925307
Darfik43 <darfik43@gmail.com>
2024-04-10 11:18:38 +0300

    + OpenWeatherAPIService Makes getLocationByName and getTemperatureByName requests to API

--- src/main/java/com/darfik/skycast/controllers/HelloServlet.java
+++ src/main/java/com/darfik/skycast/controllers/HelloServlet.java
@@ -1,9 +1,11 @@
 package com.darfik.skycast.controllers;
 
 import java.io.*;
+import java.net.URISyntaxException;
 
 import com.darfik.skycast.daos.UserDAO;
 import com.darfik.skycast.models.User;
+import com.darfik.skycast.services.OpenWeatherAPIService;
 import com.darfik.skycast.utils.HibernateUtil;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -22,15 +24,20 @@ public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
-        User newUser = new User();
-        newUser.setLogin("Test");
-        newUser.setPassword("Test");
-
-
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        String locationName = "London";
+        try {
+            String weatherData = OpenWeatherAPIService.getTemperatureByName(locationName);
+            response.getWriter()
+                    .print(
+                            "Hello\n" + "Current weather in "
+                                    + locationName + " "
+                                    + weatherData
+                    );
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void destroy() {
---
+++ src/main/java/com/darfik/skycast/services/OpenWeatherAPIService.java
@@ -0,0 +1,38 @@
+package com.darfik.skycast.services;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class OpenWeatherAPIService {
+    private static final String API_KEY = "886c310d5e5f5a4504aba96e6c34e833";
+
+    public static String getLocationByName(String locationName) throws IOException, InterruptedException, URISyntaxException {
+        String url = "http://api.openweathermap.org/geo/1.0/direct?q=" + locationName + "&appid=" + API_KEY;
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(url))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
+    }
+
+    public static String getTemperatureByName(String locationName) throws IOException, InterruptedException, URISyntaxException {
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + locationName + "&appid=" + API_KEY + "&units=metric";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(url))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
+    }
+}
