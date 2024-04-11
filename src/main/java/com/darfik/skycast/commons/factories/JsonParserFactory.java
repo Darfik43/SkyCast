db05402e192a97caa4a558a240dea5f80ea13297
Darfik43 <darfik43@gmail.com>
2024-04-11 20:16:38 +0300

    edit - return type to JsonParser<? extends ResponseJson>

--- src/main/java/com/darfik/skycast/commons/factories/JsonParserFactory.java
+++ src/main/java/com/darfik/skycast/commons/factories/JsonParserFactory.java
@@ -1,10 +1,11 @@
 package com.darfik.skycast.commons.factories;
+import com.darfik.skycast.commons.models.ResponseJson;
 import com.darfik.skycast.commons.services.JsonParser;
 import com.darfik.skycast.location.LocationJsonParser;
 import com.darfik.skycast.weather.WeatherJsonParser;
 
 public class JsonParserFactory {
-    public static JsonParser build(String type) {
+    public static JsonParser<? extends ResponseJson> build(String type) {
         return switch (type) {
             case "weather" -> new WeatherJsonParser();
             case "location" -> new LocationJsonParser();
