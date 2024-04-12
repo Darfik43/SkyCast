504b94a860fa75d5d6eeb4297f3b7a12b65ff273
Darfik43 <darfik43@gmail.com>
2024-04-12 12:26:06 +0300

    add cookies for userSession

--- src/main/java/com/darfik/skycast/user/UserRegistrationServlet.java
+++ src/main/java/com/darfik/skycast/user/UserRegistrationServlet.java
@@ -3,10 +3,7 @@ package com.darfik.skycast.user;
 import com.darfik.skycast.usersession.*;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.*;
 
 import java.io.IOException;
 
@@ -20,8 +17,9 @@ public class UserRegistrationServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String sessionId = req.getSession(true).getId();
         UserService userService = UserService.getInstance();
-        UserSessionDTO userSessionDTO = new UserSessionDTO(req.getSession().getId());
+        UserSessionDTO userSessionDTO = new UserSessionDTO(sessionId);
 
         String password = req.getParameter("password");
         String username = req.getParameter("username");
@@ -29,6 +27,10 @@ public class UserRegistrationServlet extends HttpServlet {
         UserRegistrationDTO userRegistrationDTO = new UserRegistrationDTO(username, password);
         userService.registerUser(userRegistrationDTO, userSessionDTO);
 
+        Cookie sessionCookie = new Cookie("sessionId", sessionId);
+        sessionCookie.setMaxAge(-1);
+        sessionCookie.setPath("/");
+        resp.addCookie(sessionCookie);
 
         resp.getWriter().print("You've created your account");
     }
