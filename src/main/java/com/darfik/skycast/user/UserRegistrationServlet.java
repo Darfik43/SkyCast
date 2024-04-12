fcfd4add13d8fbf4f0d3cbb95b6581f5fc02a316
Darfik43 <darfik43@gmail.com>
2024-04-12 16:53:11 +0300

    add - set userIdCookie when user is authorized

--- src/main/java/com/darfik/skycast/user/UserRegistrationServlet.java
+++ src/main/java/com/darfik/skycast/user/UserRegistrationServlet.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 
 @WebServlet("/register")
 public class UserRegistrationServlet extends HttpServlet {
+    UserServiceFactory userServiceFactory = new UserServiceFactoryImp();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -18,7 +19,7 @@ public class UserRegistrationServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String sessionId = req.getSession(true).getId();
-        UserService userService = UserService.getInstance();
+        UserService userService = userServiceFactory.build();
         UserSessionDTO userSessionDTO = new UserSessionDTO(sessionId);
 
         String password = req.getParameter("password");
@@ -26,6 +27,9 @@ public class UserRegistrationServlet extends HttpServlet {
 
         UserWithPasswordDTO userWithPasswordDTO = new UserWithPasswordDTO(username, password);
         userService.registerUser(userWithPasswordDTO, userSessionDTO);
+        String newUserId = String.valueOf(userService.getNewUserId());
+        Cookie userIdCookie = new Cookie("userId", newUserId);
+        resp.addCookie(userIdCookie);
 
         resp.getWriter().print("You've created your account");
     }
