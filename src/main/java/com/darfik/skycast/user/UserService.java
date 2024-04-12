0b12d2969cb22aea2d419b23b473ce9a2cc3dd7c
Darfik43 <darfik43@gmail.com>
2024-04-12 16:53:34 +0300

    add - authentication method

--- src/main/java/com/darfik/skycast/user/UserService.java
+++ src/main/java/com/darfik/skycast/user/UserService.java
@@ -6,29 +6,49 @@ import com.darfik.skycast.usersession.UserSessionServiceFactory;
 import com.darfik.skycast.usersession.UserSessionServiceFactoryImp;
 import com.darfik.skycast.utils.PasswordEncryptor;
 
-import java.util.Optional;
-
 public class UserService {
+
+    private User newUser;
     private final UserDAO userDAO = UserDAO.getInstance();
     private final UserSessionServiceFactory userSessionServiceFactory = new UserSessionServiceFactoryImp();
+    UserSessionService userSessionService = userSessionServiceFactory.build();
     private final PasswordEncryptor passwordEncryptor = new PasswordEncryptor();
 
 
     public void registerUser(UserWithPasswordDTO userDTO, UserSessionDTO userSessionDTO) {
         String hashedPassword = passwordEncryptor.encryptPassword(userDTO.password());
-        UserSessionService userSessionService = userSessionServiceFactory.build();
 
-        User newUser = UserMapper.toModel(userDTO);
+        newUser = UserMapper.toModel(userDTO);
         newUser.setPassword(hashedPassword);
 
         userDAO.save(newUser);
 
         userSessionService.createAndSaveUserSession(newUser, userSessionDTO);
+
+        setNewUser();
+
     }
 
-    public boolean authenticateUser(UserWithPasswordDTO userWithPasswordDTO, UserSessionDTO userSessionDTO) {
+    public void authenticateUser(UserWithPasswordDTO userWithPasswordDTO, UserSessionDTO userSessionDTO) {
+        if (isAuthenticated(userWithPasswordDTO)) {
+            newUser = UserMapper.toModel(userWithPasswordDTO);
+            setNewUser();
+            userSessionService.createAndSaveUserSession(newUser, userSessionDTO);
+        }
+
+        setNewUser();
+    }
+    private boolean isAuthenticated(UserWithPasswordDTO userWithPasswordDTO) {
         return userDAO.getByName(userWithPasswordDTO.username())
                 .map(user -> passwordEncryptor.verifyPassword(userWithPasswordDTO.password(), user.getPassword()))
                 .orElse(false);
+    }
+
+    public int getNewUserId() {
+        return newUser.getId();
+    }
+
+    private void setNewUser() {
+        userDAO.getByName(newUser.getUsername()).ifPresent(user -> newUser = user);
     }
 }
