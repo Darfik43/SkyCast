ab88baf9d035589e7ca35dc7197e03245b52136a
Darfik43 <darfik43@gmail.com>
2024-04-13 18:29:34 +0300

    register feature refactor - register service only creates new record in db

--- src/main/java/com/darfik/skycast/user/UserRegistrationServlet.java
+++ src/main/java/com/darfik/skycast/user/UserRegistrationServlet.java
@@ -4,12 +4,18 @@ import com.darfik.skycast.usersession.*;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
+import lombok.AllArgsConstructor;
 
 import java.io.IOException;
 
 @WebServlet("/register")
 public class UserRegistrationServlet extends HttpServlet {
-    private final UserServiceFactory userServiceFactory = new UserServiceFactoryImp();
+    private final UserService userService;
+
+    private UserRegistrationServlet() {
+        UserServiceFactory userServiceFactory = new UserServiceFactoryImp();
+        userService = userServiceFactory.build();
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -18,17 +24,14 @@ public class UserRegistrationServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String sessionId = req.getSession(true).getId();
-        UserService userService = userServiceFactory.build();
-        UserSessionDTO userSessionDTO = new UserSessionDTO(sessionId);
 
-        String password = req.getParameter("password");
-        String username = req.getParameter("username");
 
-        UserWithPasswordDTO userWithPasswordDTO = new UserWithPasswordDTO(username, password);
-        userService.registerUser(userWithPasswordDTO, userSessionDTO);
-        Cookie userIdCookie = new Cookie("userId", userService.getNewUserId());
-        resp.addCookie(userIdCookie);
+        UserWithPasswordDTO userWithPasswordDTO = new UserWithPasswordDTO(
+                req.getParameter("username"),
+                req.getParameter("password")
+        );
+
+        userService.registerUser(userWithPasswordDTO);
 
         resp.getWriter().print("You've created your account");
     }
--- src/main/java/com/darfik/skycast/user/UserService.java
+++ src/main/java/com/darfik/skycast/user/UserService.java
@@ -4,26 +4,24 @@ import com.darfik.skycast.usersession.*;
 import com.darfik.skycast.utils.PasswordEncryptor;
 
 public class UserService {
-
     private User newUser;
-    private final UserDAO userDAO = UserDAO.getInstance();
-    private final UserSessionServiceFactory userSessionServiceFactory = new UserSessionServiceFactoryImp();
-    private final UserSessionService userSessionService = userSessionServiceFactory.build();
-    private final PasswordEncryptor passwordEncryptor = new PasswordEncryptor();
+    private final UserDAO userDAO;
+    private final UserSessionServiceFactory userSessionServiceFactory;
+    private final UserSessionService userSessionService;
+    private final PasswordEncryptor passwordEncryptor;
+
+    UserService() {
+        userDAO = UserDAO.getInstance();
+        userSessionServiceFactory = new UserSessionServiceFactoryImp();
+        userSessionService = userSessionServiceFactory.build();
+        passwordEncryptor = new PasswordEncryptor();
+    }
 
 
-    public void registerUser(UserWithPasswordDTO userDTO, UserSessionDTO userSessionDTO) {
-        String hashedPassword = passwordEncryptor.encryptPassword(userDTO.password());
-
+    public void registerUser(UserWithPasswordDTO userDTO) {
         newUser = UserMapper.toModel(userDTO);
-        newUser.setPassword(hashedPassword);
-
+        newUser.setPassword(passwordEncryptor.encryptPassword(userDTO.password()));
         userDAO.save(newUser);
-
-        userSessionService.createAndSaveUserSession(newUser, userSessionDTO);
-
-        setNewUser();
-
     }
 
     public void authenticateUser(UserWithPasswordDTO userWithPasswordDTO, UserSessionDTO userSessionDTO) {
@@ -35,6 +33,7 @@ public class UserService {
 
         setNewUser();
     }
+
     private boolean isAuthenticated(UserWithPasswordDTO userWithPasswordDTO) {
         return userDAO.getByName(userWithPasswordDTO.username())
                 .map(user -> passwordEncryptor.verifyPassword(userWithPasswordDTO.password(), user.getPassword()))
