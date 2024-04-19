dd5216447783ee55afdfeba299193efb7e5f95ed
Darfik43 <darfik43@gmail.com>
2024-04-20 01:38:53 +0300

    SearchServlet feature(now it's hard coded)
    - Search for location by its name
    - exception handling

--- src/main/java/com/darfik/skycast/servlet/SearchServlet.java
+++ src/main/java/com/darfik/skycast/servlet/SearchServlet.java
@@ -5,19 +5,30 @@ import com.darfik.skycast.location.LocationService;
 import com.darfik.skycast.location.LocationServiceFactory;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 @WebServlet("/search")
 public class SearchServlet extends BaseServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        LocationService locationService = LocationServiceFactory.build();
-        LocationDTO locationDTO = new LocationDTO(req.getParameter("location"));
-        resp.getWriter().print(locationService.getLocationByName(locationDTO).getName());
 
+        try {
+            LocationService locationService = LocationServiceFactory.build();
+            LocationDTO locationDTO = new LocationDTO();
+            locationDTO.setName("London");
+            locationDTO = locationService.getLocationByName(locationDTO);
+            resp.getWriter().print(locationDTO.getName() + " " + locationDTO.getLatitude() + " " +
+                    locationDTO.getLongitude() + " " + locationDTO.getTemperature());
+        } catch (InterruptedException e) {
+            resp.getWriter().print("Connection to the API was interrupted");
+        } catch (IOException e) {
+            resp.getWriter().print("Can't connect to OpenWeatherAPI");
+        } catch (URISyntaxException e) {
+            resp.getWriter().print("URI is invalid");
+        }
     }
 }
