a02f5c439dccbfe167b7da843ebf7867e607a233
Darfik43 <darfik43@gmail.com>
2024-04-20 01:36:28 +0300

    LocationServlet refactoring
    - LocationServlet is "/add", and only handles add location request for user

--- src/main/java/com/darfik/skycast/servlet/LocationServlet.java
+++ src/main/java/com/darfik/skycast/servlet/LocationServlet.java
@@ -1,35 +1,36 @@
 package com.darfik.skycast.servlet;
 
+
 import com.darfik.skycast.location.LocationDTO;
 import com.darfik.skycast.location.LocationService;
 import com.darfik.skycast.location.LocationServiceFactory;
+import com.darfik.skycast.usersession.UserSessionDTO;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 
-@WebServlet(value = "/location-servlet")
+@WebServlet("/add")
 public class LocationServlet extends BaseServlet {
-    private final LocationService locationService;
-
-    public LocationServlet() {
-        locationService = LocationServiceFactory.build();
-    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        LocationDTO locationDTO = new LocationDTO();
-        locationDTO.setName(req.getParameter("locationName"));
-        locationDTO = locationService.getLocationByName(locationDTO);
-        resp.getWriter().print(locationDTO.getName() + " " + locationDTO.getLatitude() + " " +
-                locationDTO.getLongitude() + " " + locationDTO.getTemperature());
+        // To be tested
+
+        try {
+            LocationService locationService = LocationServiceFactory.build();
+            LocationDTO locationDTO = new LocationDTO(req.getParameter("locationName"));
+            UserSessionDTO userSessionDTO = new UserSessionDTO(req.getSession().getId());
+            locationService.addLocationForUser(locationDTO, userSessionDTO);
+        } catch (InterruptedException e) {
+            resp.getWriter().print("Connection to the API was interrupted");
+        } catch (IOException e) {
+            resp.getWriter().print("Can't connect to OpenWeatherAPI");
+        } catch (URISyntaxException e) {
+            resp.getWriter().print("URI is invalid");
+        }
     }
-
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    }
-
-
 }
