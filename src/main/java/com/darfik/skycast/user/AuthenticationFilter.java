2e1f56bbdcef4765b602695e22428922afc57606
Darfik43 <darfik43@gmail.com>
2024-04-19 15:39:44 +0300

    Refactor AuthFilter
    - Do all authorization transition logic between pages

--- src/main/java/com/darfik/skycast/user/AuthenticationFilter.java
+++ src/main/java/com/darfik/skycast/user/AuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.darfik.skycast.user;
 
+import com.darfik.skycast.servlet.BaseServlet;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.Cookie;
@@ -7,35 +8,32 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Objects;
+import java.util.Optional;
 
 @WebFilter("/*")
-public class AuthenticationFilter implements Filter {
+public class AuthenticationFilter extends BaseServlet implements Filter {
 
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
-
-
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) resp;
         String requestURI = request.getRequestURI().substring(request.getContextPath().length());
-        Cookie[] cookies = request.getCookies();
-
         if (("/".equals(requestURI) || "/login".equals(requestURI)) || "/register".equals(requestURI) || "/logout".equals(requestURI)) {
             chain.doFilter(request, response);
         }
 
-        Cookie sessionCookie = Arrays.stream(cookies)
-                .filter((cookie) -> Objects.equals(cookie.getName(), "sessionID"))
-                .findFirst().orElse(null);
-        if (sessionCookie != null && sessionCookie.getName().equals("sessionID")) {
-            chain.doFilter(req, resp);
-        } else {
-            response.sendRedirect(request.getContextPath() + "/login");
+        boolean isLoggedIn = false;
+        Optional<Cookie> authCookie = catchAuthCookie(request);
+        if (authCookie.isPresent()) {
+            isLoggedIn = true;
         }
-    }
 
+        request.setAttribute("isLoggedIn", isLoggedIn);
+        chain.doFilter(request, response);
+
+
+
+    }
 }
 
