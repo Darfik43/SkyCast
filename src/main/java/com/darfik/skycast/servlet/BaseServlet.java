4fff6e8ee7a9f8f5d7eb86729daf127888de6eb9
Darfik43 <darfik43@gmail.com>
2024-04-19 15:38:57 +0300

    Add BaseServlet
    BaseServlet is the servlet that we extend for other servlets, contains methods to processTemplate and catchCookie

---
+++ src/main/java/com/darfik/skycast/servlet/BaseServlet.java
@@ -0,0 +1,39 @@
+package com.darfik.skycast.servlet;
+
+import com.darfik.skycast.config.ThymeleafUtil;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
+
+public abstract class BaseServlet extends HttpServlet {
+    ITemplateEngine engine;
+
+    @Override
+    public void init() {
+        engine = ThymeleafUtil.buildTemplateEngine(this.getServletContext());
+    }
+
+    protected void processTemplate(String templateName, HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        resp.setContentType("text/html;charset=UTF-8");
+        WebContext webContext = ThymeleafUtil.buildWebContext(req, resp, getServletContext());
+
+        engine.process(templateName, webContext, resp.getWriter());
+    }
+    protected Optional<Cookie> catchAuthCookie(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null || cookies.length == 0) {
+            return Optional.empty();
+        }
+        return Arrays.stream(cookies)
+                .filter(item -> item.getName().equals("sessionID")).findFirst();
+    }
+}
+
---
+++ src/main/java/com/darfik/skycast/servlet/HomeServlet.java
@@ -0,0 +1,20 @@
+package com.darfik.skycast.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/home")
+public class HomeServlet extends BaseServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.processTemplate("home", req, resp);
+    }
+
+
+}
