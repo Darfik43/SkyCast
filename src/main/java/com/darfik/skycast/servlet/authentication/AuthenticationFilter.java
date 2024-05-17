a682faf04fefecc5fbf25bd1a428264148b3a807
Darfik43 <darfik43@gmail.com>
2024-05-17 12:06:39 +0300

    AuthenticationFilter change
    Filter is only responsible for processing authentication between requests
    
    - Delete redirect logic
    - the if statement is replaced with simply boolean expression

--- src/main/java/com/darfik/skycast/servlet/authentication/AuthenticationFilter.java
+++ src/main/java/com/darfik/skycast/servlet/authentication/AuthenticationFilter.java
@@ -1,7 +1,7 @@
 package com.darfik.skycast.servlet.authentication;
 
-import com.darfik.skycast.model.UserSession;
 import com.darfik.skycast.dao.UserSessionDAO;
+import com.darfik.skycast.model.UserSession;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.Cookie;
@@ -15,36 +15,14 @@ import java.util.Optional;
 public class AuthenticationFilter extends CookieServlet implements Filter {
      private final UserSessionDAO userSessionDAO = UserSessionDAO.getInstance();
 
-
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) resp;
-        String requestURI = request.getRequestURI().substring(request.getContextPath().length());
 
-        if ("/".equals(requestURI)) {
-            ((HttpServletResponse) resp).sendRedirect(((HttpServletRequest) req).getContextPath() + "/home");
-            return;
-        }
-
-        if ("/register".equals(requestURI) || "/logout".equals(requestURI)) {
-            chain.doFilter(request, response);
-            return;
-        }
-
-
-
-        boolean isLoggedIn = false;
         Optional<Cookie> authCookie = catchAuthCookie(request);
-        if (authCookie.isPresent()
-            && !userSessionDAO.isExpired(new UserSession(authCookie.toString()))) {
-            isLoggedIn = true;
-        }
-
-        if ("/search".equals(requestURI) && !isLoggedIn) {
-            req.getRequestDispatcher("/templates/login.html").forward(req, resp);
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-        }
+        boolean isLoggedIn = authCookie.isPresent()
+                && !userSessionDAO.isExpired(new UserSession(authCookie.toString()));
 
         request.setAttribute("isLoggedIn", isLoggedIn);
         chain.doFilter(request, response);
