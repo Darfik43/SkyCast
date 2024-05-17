2c19405bf13179882df41bf1603e807a4cdbc863
Darfik43 <darfik43@gmail.com>
2024-05-17 12:38:02 +0300

    SearchServlet change:
    - Add logic to check ifLocationAlreadyAdded and display it

--- src/main/java/com/darfik/skycast/servlet/SearchServlet.java
+++ src/main/java/com/darfik/skycast/servlet/SearchServlet.java
@@ -1,6 +1,7 @@
 package com.darfik.skycast.servlet;
 
 import com.darfik.skycast.model.dto.LocationDTO;
+import com.darfik.skycast.model.dto.UserDTO;
 import com.darfik.skycast.service.LocationService;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,23 +15,31 @@ public class SearchServlet extends RenderServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         try {
-            LocationService locationService = new LocationService();
             LocationDTO locationDTO = new LocationDTO(req.getParameter("searchQuery"));
+            LocationService locationService = new LocationService();
+
+            boolean locationAlreadyAdded = locationService.locationExistsForUser(
+                    locationDTO,
+                    new UserDTO(req.getSession().getAttribute("username").toString())
+            );
+            req.setAttribute("locationAlreadyAdded", locationAlreadyAdded);
+
             locationDTO = locationService.getLocationByName(locationDTO);
             locationDTO = locationService.getWeatherByCoordinates(locationDTO);
-
             req.setAttribute("location", locationDTO.getName());
-            req.setAttribute("latitude", locationDTO.getLatitude());
-            req.setAttribute("longitude", locationDTO.getLongitude());
             req.setAttribute("temperature", locationDTO.getTemperature());
 
             processTemplate("search_results.html", req, resp);
+
         } catch (InterruptedException e) {
             resp.getWriter().print("Connection to the API was interrupted");
         } catch (IOException e) {
             resp.getWriter().print("Can't connect to OpenWeatherAPI");
         } catch (URISyntaxException e) {
             resp.getWriter().print("URI is invalid");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            req.setAttribute("errorMessage", "No locations found");
+            super.processTemplate("error", req, resp);
         }
     }
 }
