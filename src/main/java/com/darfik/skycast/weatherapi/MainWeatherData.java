236be661193339b84a34742c4348c6c1c31d15fb
Darfik43 <darfik43@gmail.com>
2024-05-19 00:26:18 +0300

    MainWeatherData change:
    - returns Double instead of BigDecimal

--- src/main/java/com/darfik/skycast/weatherapi/MainWeatherData.java
+++ src/main/java/com/darfik/skycast/weatherapi/MainWeatherData.java
@@ -4,17 +4,14 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
-import java.math.BigDecimal;
-
 
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class MainWeatherData {
 
     @JsonProperty("temp")
-    private BigDecimal temp;
+    private Double temp;
 
     @JsonProperty("feels_like")
-    private BigDecimal feelsLike;
-
+    private Double feelsLike;
 }
