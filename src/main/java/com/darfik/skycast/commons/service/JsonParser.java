410321f45278d31ea63a99532440c950d1fd8df1
Darfik43 <darfik43@gmail.com>
2024-05-19 23:17:26 +0300

    Delete Parser interface

--- src/main/java/com/darfik/skycast/commons/service/JsonParser.java
+++
@@ -1,6 +0,0 @@
-package com.darfik.skycast.commons.service;
-
-
-public interface JsonParser<T> {
-    T parse(String json);
-}
--- src/main/java/com/darfik/skycast/util/json/WeatherJsonParser.java
+++ src/main/java/com/darfik/skycast/util/json/WeatherJsonParser.java
@@ -1,13 +1,11 @@
 package com.darfik.skycast.util.json;
-import com.darfik.skycast.commons.service.JsonParser;
 import com.darfik.skycast.weatherapi.WeatherJson;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 
-public class WeatherJsonParser implements JsonParser<WeatherJson> {
+public class WeatherJsonParser {
     private final JsonMapper jsonMapper = JsonMapper.builder().build();
 
-    @Override
     public WeatherJson parse(String json) {
         try {
             return jsonMapper.readValue(json, WeatherJson.class);
