c42849b37f5d8fdb60744ac818b13aa2eaadfc8c
Darfik43 <darfik43@gmail.com>
2024-05-19 23:18:07 +0300

    Change parse method:
    - Now return an array[up to 5] of found location

--- src/main/java/com/darfik/skycast/util/json/LocationJsonParser.java
+++ src/main/java/com/darfik/skycast/util/json/LocationJsonParser.java
@@ -1,17 +1,16 @@
 package com.darfik.skycast.util.json;
 
-import com.darfik.skycast.commons.service.JsonParser;
 import com.darfik.skycast.weatherapi.LocationJson;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 
-public class LocationJsonParser implements JsonParser<LocationJson> {
+public class LocationJsonParser {
 
     private final JsonMapper jsonMapper = JsonMapper.builder().build();
-    @Override
-    public LocationJson parse(String json) {
+
+    public LocationJson[] parse(String json) {
         try {
-            return jsonMapper.readValue(json, LocationJson[].class)[0];
+            return jsonMapper.readValue(json, LocationJson[].class);
         } catch (JsonProcessingException e) {
             throw new RuntimeException("Error parsing location JSON", e);
         } catch (ArrayIndexOutOfBoundsException e) {
